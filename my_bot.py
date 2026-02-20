9fd97109e68a372bbe1a9ef13c4592e380b525af
eaglesareedible <zadler2029@kehillahstudent.org>
2026-02-20 11:55:17 -0800

    C

--- my_bot.py
+++ my_bot.py
@@ -10,11 +10,18 @@ This function will be called every time anyone says anything on a channel where 
 * You can have certain words or patterns in the messages trigger the bot.
 * You can have the bot respond differently to different users
 """
+
+userInput=["bad", "no", "hello", "yes", "what is dodo's name?", "how are you?", 'what can you do?', 'keo']
+botResponse=["That's bad!", "Aww, that's a shame", "Hi! I'm here.","Agreed","Dodo's name is Ido Tsoref. Google it!", "I'm good, how are you?","I can say hi when you say my name, or Keo's name, I can let you know how I'm doing, and I can tell you wha time it is!", "haha, keo"]
+
+
+
 def should_i_respond(user_message, user_name):
-  if "robot" in user_message:
-    return True
-  else:
-    return False
+  for i in userInput:
+    if f"{i}" in user_message:
+      return True
+  return False
+ 
 
 """
 **Do NOT change the name of this function.**
@@ -26,9 +33,7 @@ This function will be called every time the `should_i_respond` function returns 
 * You can have the bot respond differently to different messages and users
 """
 def respond(user_message, user_name):
-  return f"""you said my name!!
-  {user_message.replace("robot", user_name)}"""
-
-userInput=["bad", "no", "hello", "yes", "what is dodo's name", "how are you", 'what can you do', 'keo']
-botResponse=["That's bad!", "Aww, that's a shame", "Hi! I'm here.","Agreed","Dodo's name is Ido Tsoref. Google it!", "I'm good, how are you?","I can say hi when you say my name, or Keo's name, I can let you know how I'm doing, and I can tell you wha time it is!", "haha, keo"]
+  var = userInput.index(user_message)
+  return f"{botResponse[var]}"""
+  {user_message.replace("robot", user_name)}
 
