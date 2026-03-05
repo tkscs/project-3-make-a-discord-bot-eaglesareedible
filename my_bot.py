3abc2e204b277391517b3870d3834cdaf4454cf8
eaglesareedible <zadler2029@kehillahstudent.org>
2026-03-05 08:28:50 -0800

    c

--- my_bot.py
+++ my_bot.py
@@ -12,6 +12,14 @@ This function will be called every time anyone says anything on a channel where 
 * You can have certain words or patterns in the messages trigger the bot.
 * You can have the bot respond differently to different users
 """
+results = ["🍒", "7", "🍇", "🍓", "🍊", "🍷"]
+
+def slots():
+  slotspot1 = random.choice(results)
+  slotspot2 = random.choice(results)
+  slotspot3 = random.choice(results)
+  return f"{slotspot1}, {slotspot2}, {slotspot3}"
+
 def coinflip():
   headsTails=random.randint(1, 2)
   if headsTails==1:
@@ -20,8 +28,8 @@ def coinflip():
     return "Tails"
   
 currenttime=time.asctime(time.localtime())
-userInput=["bad", "no", "hello", "yes", "what is dodos name", "how are you", 'what can you do', 'keo', "what time is it", "coinflip"]
-botResponse=["That's bad!", "Aww, that's a shame", "Hi! I'm here.","Agreed","Dodo's name is Ido Tsoref. Google it!", "I'm good, how are you?","I can say hi when you say my name, or Keo's name, I can let you know how I'm doing, and I can tell you what time it is! And I can flip a coin", "haha, keo", f"{currenttime}", f"{coinflip()}"]
+userInput=["bad", "no", "hello", "yes", "what is dodos name", "how are you", 'what can you do', 'keo', "what time is it", "coinflip", "slotmachine"]
+botResponse=["That's bad!", "Aww, that's a shame", "Hi! I'm here.","Agreed","Dodo's name is Ido Tsoref. Google it!", "I'm good, how are you?","I can say hi when you say my name, or Keo's name, I can let you know how I'm doing, and I can tell you what time it is! And I can flip a coin", "haha, keo", f"{currenttime}", f"{coinflip()}", f"{slots()}"]
 
 ALPA = string.ascii_lowercase + " "
 
@@ -36,12 +44,11 @@ def should_i_respond(user_message, user_name):
       return True
   return False
  
-
-
 #Do NOT change the name of this function.
 
 
 def respond(user_message, user_name):
+  currenttime=time.asctime(time.localtime())
   botResponse=["That's bad!", "Aww, that's a shame", "Hi! I'm here.","Agreed","Dodo's name is Ido Tsoref. Google it!", "I'm good, how are you?","I can say hi when you say my name, or Keo's name, I can let you know how I'm doing, and I can tell you what time it is! And I can flip a coin", "haha, keo", f"{currenttime}", f"{coinflip()}"]
   whitelisteed_user_message=""
   editeduser_message=user_message.lower()
